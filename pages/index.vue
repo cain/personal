2a43c38f2c3dfac7b83c4464a9b3d7459338f2d0
Cain Hall <cain.hall98@gmail.com>
2018-04-03 12:03:38 +1000

    Clean up css, start design

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,26 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
+    <div class="section-nav">
+      <a href="#">home</a>
+      <a href="#">work</a>
+      <a href="#">blog</a>
+      <a href="#">contact</a>
+    </div>
+    <div class="section-banner">
       <h1 class="title">
-        personal
+        Something Cool Here
       </h1>
-      <h2 class="subtitle">
-        My personal website
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <p class="sub-title">
+        Tumblr polaroid DIY tilde green juice bicycle rights. Farm-to-table vape lo-fi tote bag literally salvia. Taiyaki gluten-free roof party aesthetic slow-carb swag banh mi gentrify cronut bicycle rights. Green juice whatever microdosing shoreditch, tote bag slow-carb jianbing hashtag gentrify swag photo booth edison bulb.
+      </p>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-  components: {
-    AppLogo
-  }
+
 }
 </script>
 
@@ -36,18 +28,59 @@ export default {
 .container {
   min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
+  flex-direction: column;
+  max-width: 1000px;
+  margin: auto;
+}
+
+.section-nav {
+  text-align: left;
+  margin-top: 6px;
+  position: fixed;
+}
+.section-nav a { 
+  margin-right: 30px;
+  font-family: LucidaGrande;
+  font-size: 14px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #222222;
+}
+.section-banner {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  margin-top: 30vh;
+}
+
+.sub-title {
+  font-family: "Source Code Pro", "monaco", "PT Mono", "Courier", monospace;
+  font-size: 12px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #222222;
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  font-family: Georgia;
+  font-size: 40px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #222222;
+  margin: 12px 0px;
 }
 
 .subtitle {
